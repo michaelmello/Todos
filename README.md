a83cf196d1159c417c6e88c5c008cf8ccf7ef652
Michael Mello <michael.mello@gmail.com>
2021-08-25 16:52:06 -0400

    Initial Razor Pages creation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Todos
-Various sample Todo Implementations with ASP.NET Core.
+Various sample todo implementations with ASP.NET Core.
 
 ## How to Use
 [Trust the ASP.NET Core HTTPS development certificate on Windows and macOS.](https://docs.microsoft.com/en-us/aspnet/core/security/enforcing-ssl?view=aspnetcore-5.0&tabs=visual-studio#trust-the-aspnet-core-https-development-certificate-on-windows-and-macos)
---
+++ TodoWithRazorPages/Pages/Index.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TodoWithRazorPages.Models;
+
+namespace TodoWithRazorPages.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    // [BindProperty]
+    // public List<Todo> Todos { get; set; }
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
